19fcdbbe72841cc3d8b90c8ae6318e1da4258f46
Ahmed Abdalla <>
2022-08-27 07:14:38 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,31 @@
+// and = &&
+// or = ||
+// not = !
+// c++ =c+1
+// a ??= 20 لو المتغير ليس له قيمه خاليه 20
+// a -= 10 , a /= 5 , a *= 5  
+// for (var = value ; condtion ; operator) {code}
+//for loop
+
+void main(){
+ // int year =1900 ;
+//while (year <= 2020){
+ // year++;
+ // print(year.isOdd);
+  
+//}
+
+  
+  int year;
+for (year = 2000 ; year >= 1900 ; year--){
+  print(year);
+}
+
+  
+//do {
+//  print(year);
+//  year++ ;
+//}//while(year <= 2000 );
+
+
+}
